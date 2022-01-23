8b70ded19cf6c202cff38ad023de4c033bf110a8
abhigandhi29 <abhigndhi29@gmail.com>
2022-01-23 19:19:43 +0530

    making changes in c

--- Assignment1/Assgn1_1c.sh
+++ Assignment1/Assgn1_1c.sh
@@ -1,1 +1,14 @@
-for file in $(find $1 -type f);do echo "${file##*.}";[[ "${file##*.}" == "$file" ]]&&{ mkdir -p Nil;find "$1" -type f ! -name "*.*" -exec mv {} ./Nil \; ; }||{ mkdir -p "${file##*.}";find "$1" -type f -name "*.${file##*.}" -exec mv {} "./${file##*.}" \; ; };done
+extensions=($(find $1 -type f -name "*.*" | awk -F. '{print $NF}' | sort -u))
+for extension in ${extensions[@]}; do
+    echo $extension
+    mkdir -p $extension 
+    files=($(find $1 -type f -name "*.$extension"))
+    for ((i=0; i<${#files[*]}; i+=1000)); do
+        mv ${files[@]:i:1000} $extension
+    done  
+done
+mkdir -p Nil 
+files=($(find $1 -type f))
+for ((i=0; i<${#files[*]}; i+=1000)); do
+    mv ${files[@]:i:1000} Nil
+done  
--- Assignment1/assign1_1c.sh
+++
@@ -1,5 +0,0 @@
-for file in $(find $1 -type f -name "*")
-do
-    echo "${file##*.}"
-    [[ "${file##*.}" == "$file" ]] && { mkdir -p Nil ; find "$1" -type f ! -name "*.*" -exec mv {} ./Nil \; ; } || { mkdir -p "${file##*.}" ; find "$1" -type f -name "*.${file##*.}" -exec mv {} "./${file##*.}" \; ; } 
-done
